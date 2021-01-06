2ab1e1d5e54d2040c70752aee463274ea6bfb49d
Tony Zhao <tonyzhao@lyncwork.com>
2021-01-05 18:11:00 -0800

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { Header } from "./Components";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <div>homePAge</div>
+        </Route>
+        <Route path="/button" exact>
+          <div>button</div>
+        </Route>
+        <Route path="/form" exact>
+          <div>form</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
-  height: 100px;
-  background-color: pink;
-  margin: 0;
+
+  padding: 30px;
+  border-bottom: 1px solid black;
+
+  display: flex;
+  justify-content: space-around;
 `;
 
 const Header = () => {
-  return <Container>some stuff</Container>;
+  return (
+    <Container>
+      <Link to="/">home</Link>
+      <Link to="/button">button page</Link>
+      <Link to="/form">formpage</Link>
+    </Container>
+  );
 };
 
 export default Header;
